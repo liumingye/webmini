b99f188dc4450dd2236a580548e8ed222590b966
liumingye <898310895@qq.com>
2022-03-11 14:54:30 +0800

    feat: 修改log文件名，更新log样式

--- packages/common/logger.ts
+++ packages/common/logger.ts
@@ -7,21 +7,28 @@ import node_console from 'console'
 const electron = is.renderer() ? require('@electron/remote') : require('electron')
 const level = is.dev() ? 'debug' : 'info'
 const logDir = electron.app.getPath('logs')
+const logName = `${electron.app.name}-%DATE%.log`
 
 export default createLogger({
   level,
+  format: format.timestamp({ format: 'YYYY-MM-dd HH:mm:ss.ms' }),
   transports: [
     new transports.DailyRotateFile({
       level: 'debug',
-      filename: resolve(logDir, '%DATE%.log'),
+      filename: resolve(logDir, logName),
       datePattern: 'YYYY-MM-DD',
       zippedArchive: true,
+      json: false,
       maxSize: '10m',
       maxFiles: '7d',
-      format: format.combine(format.timestamp(), format.json()),
+      format: format.combine(
+        format.printf(({ level, message, label, timestamp }) => {
+          return `${timestamp} ${label ? `[${label}] ` : ''}${level}: ${message}`
+        }),
+      ),
     }),
     new transports.Console({
-      format: format.combine(format.colorize({ all: true }), format.timestamp()),
+      format: format.combine(format.colorize({ all: true })),
       log: ({ level, message, label, timestamp }, next) => {
         const print = `${timestamp} ${label ? `[${label}] ` : ''}${level}: ${message}`
         console.log(print)
